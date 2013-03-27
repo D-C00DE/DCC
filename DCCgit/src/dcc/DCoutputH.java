6c917affc7fadbc9d2b83796f37f7b1098984de3
dusakus <>
2013-03-27 15:33:35 +0000

    DCoutputH: variable rename :P

--- DCCgit/src/dcc/DCoutputH.java
+++ DCCgit/src/dcc/DCoutputH.java
@@ -20,45 +20,45 @@ public class DCoutputH {
 	time = new Date();
 	debug = debugI; 
     }
-    public boolean println (String intput){
-		System.out.println("[" + timed.format(time) + "] " + intput);
+    public boolean println (String input){
+		System.out.println("[" + timed.format(time) + "] " + input);
 		return true;
 	}
-    public boolean println (String intput,String mode){
+    public boolean println (String input,String mode){
 		switch (mode) {
                     case "N":
-                        this.println(intput);
+                        this.println(input);
                     case "E1":
                         //for a little error
                         this.println("[E1] Whops ...");
-                        this.println("[E1]=> " + intput);
+                        this.println("[E1]=> " + input);
                         this.println("[E1] of course it's still working :)");
                     case "E2":
                         this.println("[E2] Something went wrong ...");
-                        this.println("[E2]=> " + intput);
+                        this.println("[E2]=> " + input);
                         this.println("[E2] but it didn't crashed yet ;)");
                     case "E3":
                         this.println("[E3] I have very bad news");
-                        this.println("[E3]=> " + intput);
+                        this.println("[E3]=> " + input);
                         this.println("[E3] <hardly continues it's work>");
                     case "E4":
                         this.println("[E4] I'm so sorry...");
-                        this.println("[E4]=> " + intput);
+                        this.println("[E4]=> " + input);
                         this.println("[E4] i'm dying !");
                         try {this.wait(1000);} catch (InterruptedException ex){}
                         this.println("BZZt Bzzt bzz...");
                         System.exit(2);
                     case "E5":
                         this.println("[E5] Critical error ocured:");
-                        this.println("[E5]=> " + intput);
+                        this.println("[E5]=> " + input);
                         this.println("[E5] Killing in 1 second");
                         try {this.wait(1000);} catch (InterruptedException ex){}
                         this.println("!!!ERROR!!!");
                         System.exit(2);
                     case "D":
-                        if(debug){this.println("[Debug]=> " + intput);}
+                        if(debug){this.println("[Debug]=> " + input);}
                     case "Debug":
-                        if(debug){this.println("[Debug]=> " + intput);}
+                        if(debug){this.println("[Debug]=> " + input);}
 
     }
                 return true;
