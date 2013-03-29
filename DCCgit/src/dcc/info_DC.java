246a9a5fde6f41d29762341ee5d0e2cce236d6f8
dusakus <dusakus@D-pc>
2013-03-29 21:36:05 +0100

    ver 0.0.1 reached [YEAH]

--- DCCgit/src/dcc/info_DC.java
+++ DCCgit/src/dcc/info_DC.java
@@ -3,8 +3,8 @@ package dcc;
 public class info_DC {
     //core data
 	static String title = "D-code-core";
-	static String ver = "0.0.0 [pre1]";
-	static String date = "2013-3-16";
+	static String ver = "0.0.1";
+	static String date = "2013-3-29";
     //debug config
         static String mode = "gameC";
 }
